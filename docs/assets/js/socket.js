c70ad3390b80e28dc65052725028d21cc6715450
Kevin Martin <kevin@kevsbox.com>
2022-09-12 10:15:52 +0100

    Update socket.js

--- docs/assets/js/socket.js
+++ docs/assets/js/socket.js
@@ -1,15 +1,15 @@
 const socket = new SockJS('https://tasktrackerserver.herokuapp.com/time')
 var stompClient = Stomp.over(socket)
 
+stompClient.connect({}, function (frame) {}, function(message) {
+
+})
+
 
 function subscribe(callback, id) {
-  stompClient.connect({}, function (frame) {
     stompClient.subscribe('/topic/timer', (message) => {
       callback(JSON.parse(message.body))
     }, {id})
-  }, function(message) {
-    subscribe(callback, id)
-  })
 }
 
 function unsubscribe(id) {
